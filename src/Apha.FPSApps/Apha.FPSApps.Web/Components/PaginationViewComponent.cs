d1464bffe50c089857c5ca286bff204745bf41e4
chougaleakshay <akshay.chougale@atos.net>
2026-02-27 16:24:31 +0530

    feat: Added datagrid component files.

---
+++ src/Apha.FPSApps/Apha.FPSApps.Web/Components/PaginationViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Apha.FPSApps.Web.Models.Components.DataGrid;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apha.FPSApps.Web.Components
+{
+    public class PaginationViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(PaginationModel pagination)
+        {
+            return View(pagination);
+        }
+    }
+}
---
+++ src/Apha.FPSApps/Apha.FPSApps.Web/Models/Components/DataGrid/GridHelpers.cs
@@ -0,0 +1,70 @@
+﻿namespace Apha.FPSApps.Web.Models.Components.DataGrid
+{
+    public class GridHelpers
+    {
+        public static object GetPropertyValue(object obj, string propertyName)
+        {
+            try
+            {
+                if (obj == null) return null;
+                var type = obj.GetType();
+                var prop = type.GetProperty(propertyName);
+                return prop?.GetValue(obj);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static string GetTypeCssClass(DataGridColumn column)
+        {
+            return column.ColumnType switch
+            {
+                GridColumnType.Date => "grid-readonly",
+                GridColumnType.DateTime => "grid-readonly",
+                //GridColumnType.Decimal => "grid-readonly",
+                GridColumnType.Number => "grid-readonly",
+                GridColumnType.Text => "grid-readonly",
+                GridColumnType.Dropdown => "grid-readonly",
+                GridColumnType.Checkbox => "grid-input grid-checkbox",
+                _ => ""
+            };
+        }
+
+        public static string FormatValue(object value, DataGridColumn column)
+        {
+            if (value == null) return "";
+
+            switch (column.ColumnType)
+            {
+                case GridColumnType.Date:
+                    if (value is DateTime dateValue)
+                        return dateValue.ToString(column.DateFormat ?? "yyyy-MM-dd");
+                    break;
+                case GridColumnType.DateTime:
+                    if (value is DateTime dateTimeValue)
+                        return dateTimeValue.ToString(column.DateFormat ?? "yyyy-MM-dd HH:mm");
+                    break;
+                // case GridColumnType.Decimal:
+                //     if (value is decimal decValue)
+                //         return decValue.ToString(column.DecimalFormat ?? "F2");
+                //     break;
+                // case GridColumnType.Number:
+                //     if (value is double dblValue)
+                //         return dblValue.ToString(column.DecimalFormat ?? "F2");
+                //     else if (value is float fltValue)
+                //         return fltValue.ToString(column.DecimalFormat ?? "F2");
+                //     else if (value is int intValue)
+                //         return intValue.ToString();
+                //     break;
+                // case GridColumnType.Checkbox:
+                //     return Convert.ToBoolean(value) ? "☑" : "☐";
+                default:
+                    return value?.ToString() ?? string.Empty;
+            }
+
+            return value?.ToString() ?? string.Empty; ;
+        }
+    }
+}
