744bff1fa5b8b9734665728b95be1b4af9a46d6e
rohit2agarwal <rohit.2.agarwal@atos.net>
2026-03-10 11:17:11 +0530

    feat: Add doc for setting controller

--- src/Apha.FPS/Apha.FPS.Api/Controllers/FpsSettingController.cs
+++ src/Apha.FPS/Apha.FPS.Api/Controllers/FpsSettingController.cs
@@ -1,11 +1,16 @@
 ﻿using Apha.Common.Contracts.FPS;
 using Apha.FPS.Application.Interfaces;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Apha.FPS.Api.Controllers
 {
+    /// <summary>
+    /// API controller for managing FPS settings.
+    /// </summary>
+    [Authorize(Roles = "API-FPSUser,API-FPSAdmin")]
     [ApiController]
     [Route("api/setting")]
     public class FpsSettingController : ControllerBase
@@ -13,6 +18,11 @@ namespace Apha.FPS.Api.Controllers
         private readonly IFpsSettingService _fpsSettingService;
         private readonly IMapper _mapper;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FpsSettingController"/> class.
+        /// </summary>
+        /// <param name="fpsSettingService">The FPS setting service.</param>
+        /// <param name="mapper">The AutoMapper instance.</param>
         public FpsSettingController(
                         IFpsSettingService fpsSettingService,
                         IMapper mapper)
@@ -21,6 +31,10 @@ namespace Apha.FPS.Api.Controllers
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// Gets all FPS settings.
+        /// </summary>
+        /// <returns>A list of FPS settings.</returns>
         [HttpGet]
         public async Task<IActionResult> GetAsync()
         {
