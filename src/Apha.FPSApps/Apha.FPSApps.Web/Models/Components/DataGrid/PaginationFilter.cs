6f130f15e9261a2d16f521e54209eaefb05ea2bf
chougaleakshay <akshay.chougale@atos.net>
2026-03-19 17:52:56 +0530

    feat: Sonar fixes for DataGird files.

--- src/Apha.FPSApps/Apha.FPSApps.Web/Models/Components/DataGrid/PaginationFilter.cs
+++ src/Apha.FPSApps/Apha.FPSApps.Web/Models/Components/DataGrid/PaginationFilter.cs
@@ -6,7 +6,7 @@
         public int Page { get; set; } = 1;
         public int PageSize { get; set; } = 10;
         public string? SortBy { get; set; }
-        public bool Descending { get; set; }
+        public bool Descending { get; set; } = false;
         public TFilter? Filter { get; set; }
     }
 }
