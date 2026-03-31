a070993075a9984a80b61d0a31f441a0534962aa
Sejal Dhotre <sejal.dhotre@atos.net>
2026-03-31 08:03:49 +0000

    feat:PIMS basic project structure updated.

---
+++ src/Apha.FPSApps/Apha.FPSApps.Application.UnitTests/PIMS/ProjectServiceTest/ProjectServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apha.FPSApps.Application.UnitTests.PIMS.YourServiceNametServiceTest
+{
+    internal class YourServiceNameServiceTests
+    {
+    }
+}
---
+++ src/Apha.FPSApps/Apha.FPSApps.Web.UnitTests/PIMS/Controllers/HomeControllerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apha.FPSApps.Web.UnitTests.PIMS.Controllers
+{
+    internal class YourControllerNameControllerTests
+    {
+    }
+}
