ae206a525838486adb1a0e853cea43202c0aa5a9
VedpathakMeghana <meghana.vedpathak@atos.net>
2026-04-08 11:00:55 +0530

    feat: costbook select and edit project UI

---
+++ src/Apha.FPSApps/Apha.FPSApps.Application/Dtos/CostBook/ContractDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apha.FPSApps.Application.Dtos.CostBook
+{
+    public class ContractDto
+    {
+        [Required]
+        public string ContractNumber { get; set; } = string.Empty;
+
+        public string? ContractName { get; set; }
+
+        public string? Customer { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public decimal? ContractValue { get; set; }
+
+        public string Status { get; set; } = "Active";
+    }
+}
---
+++ src/Apha.FPSApps/Apha.FPSApps.Application/Dtos/CostBook/ProjectFilterDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apha.FPSApps.Application.Dtos.CostBook
+{
+    public class ProjectFilterDto
+    {
+        public string? ContractFilter { get; set; }
+        public string? SubmittedByFilter { get; set; }
+        public string? SearchTerm { get; set; }
+        public int Year { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
+    }
+}
