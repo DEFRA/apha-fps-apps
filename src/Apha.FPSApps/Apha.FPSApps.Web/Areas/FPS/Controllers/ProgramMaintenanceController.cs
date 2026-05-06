7b346807cc46924d0a4bb3b378b03fd6b6844fd0
Ken0212JR <146068102+Ken0212JR@users.noreply.github.com>
2026-05-06 18:24:40 +0530

    feat : code changes for internal issues for the Release branch (#66)

--- src/Apha.FPSApps/Apha.FPSApps.Web/Areas/FPS/Controllers/ProgramMaintenanceController.cs
+++ src/Apha.FPSApps/Apha.FPSApps.Web/Areas/FPS/Controllers/ProgramMaintenanceController.cs
@@ -98,6 +98,10 @@ namespace Apha.FPSApps.Web.Areas.FPS.Controllers
                 });
             }
 
+            // Ensure Target is always stored as a positive value
+            if (model.Target.HasValue)
+                model.Target = Math.Abs(model.Target.Value);
+
             var dto = _mapper.Map<ProgramDto>(model);
             var response = await _programService.AddProgramAsync(dto);
             if (response.Success)
@@ -147,6 +151,10 @@ namespace Apha.FPSApps.Web.Areas.FPS.Controllers
                         }))
                 });
             }
+
+            // Ensure Target is always stored as a positive value
+            if (model.Target.HasValue)
+                model.Target = Math.Abs(model.Target.Value);
 
             var dto = _mapper.Map<ProgramDto>(model);
             var response = await _programService.UpdateProgramAsync(dto);
@@ -207,6 +215,8 @@ namespace Apha.FPSApps.Web.Areas.FPS.Controllers
                 programItems = _mapper.Map<List<ProgramViewModel>>(response.Data.ToList());
             }
             PaginationModel paginationModel = _mapper.Map<PaginationModel>(response.Pagination) ?? new PaginationModel();
+            paginationModel.SortColumn = query?.SortBy;
+            paginationModel.SortDirection = query?.Descending ?? false;
 
             var programGridConfig = new DataGridConfig<ProgramViewModel>
             {
