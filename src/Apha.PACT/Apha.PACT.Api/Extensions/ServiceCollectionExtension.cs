825562ddd29003da2dacb5fdfe8814b7d57f35de
atos.user11 <atos.user11@EC2AMZN-SREA9GO>
2026-05-08 13:51:08 +0530

    feat: removed duplicate code

--- src/Apha.PACT/Apha.PACT.Api/Extensions/ServiceCollectionExtension.cs
+++ src/Apha.PACT/Apha.PACT.Api/Extensions/ServiceCollectionExtension.cs
@@ -32,8 +32,6 @@ namespace Apha.PACT.Api.Extensions
             services.AddScoped<ITestorProductService, TestorProductService>();
             services.AddScoped<IProjectMonthService, ProjectMonthService>();
             services.AddScoped<IProjectProfileService, ProjectProfileService>();
-            services.AddScoped<IProjectMonthService, ProjectMonthService>();
-            services.AddScoped<IProjectProfileService, ProjectProfileService>();
             return services;
         }
         public static IServiceCollection AddRepositories(this IServiceCollection services)
@@ -54,8 +52,7 @@ namespace Apha.PACT.Api.Extensions
             services.AddScoped<IProjectMonthRepository, ProjectMonthRepository>();
             services.AddScoped<IProjectProfileRepository, ProjectProfileRepository>();
 
-
-                 return services;
+            return services;
         }
     }
 }
