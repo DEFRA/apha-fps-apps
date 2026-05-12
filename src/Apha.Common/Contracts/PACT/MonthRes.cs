1033cee6924c047e04840d13ccd6bafe39ec377f
atos.user11 <atos.user11@EC2AMZN-SREA9GO>
2026-05-12 10:40:12 +0530

    feat: revert  monthDTO and MonthRes

--- src/Apha.Common/Contracts/PACT/MonthRes.cs
+++ src/Apha.Common/Contracts/PACT/MonthRes.cs
@@ -4,7 +4,5 @@ namespace Apha.Common.Contracts.PACT
     {
         public required short MonthNumber { get; set; }
         public required string MonthName { get; set; }
-        public short? AccntsPeriod { get; set; }
-        public short? FQuarter { get; set; }
     }
 }
