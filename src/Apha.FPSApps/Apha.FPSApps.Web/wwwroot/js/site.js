cd89476d765f22c0efda4363b5a54215842b5ee5
chougaleakshay <akshay.chougale@atos.net>
2026-05-14 10:34:06 +0530

    feat: Fixed PACT issues.

--- src/Apha.FPSApps/Apha.FPSApps.Web/wwwroot/js/site.js
+++ src/Apha.FPSApps/Apha.FPSApps.Web/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Strip leading zeros from all number inputs on change (e.g. 013 → 13)
+$(document).on('change', 'input[type="number"]', function () {
+    if (this.value !== '') this.value = +this.value;
+});
