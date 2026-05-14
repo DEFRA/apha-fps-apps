5a8532bad28234422c3ff4d9609045012a9d4d2e
atos.user11 <atos.user11@EC2AMZN-SREA9GO>
2026-05-14 21:54:34 +0530

    feat: Sonar issue fixes

--- src/Apha.FPS/Apha.FPS.DataAccess/Repositories/EmployeeRepository.cs
+++ src/Apha.FPS/Apha.FPS.DataAccess/Repositories/EmployeeRepository.cs
@@ -273,22 +273,22 @@ namespace Apha.FPS.DataAccess.Repositories
             var dict = (IDictionary<string, object>)filterModel;
 
             if (dict.TryGetValue("PactId", out var pactId) && pactId != null)
-                query = query.Where(x => EF.Functions.Like(x.PactId!.ToLower(), $"%{pactId.ToString()!.ToLower()}%"));
+                query = query.Where(x => x.PactId != null && x.PactId.ToLower().Contains(pactId.ToString()!.ToLower()));
 
             if (dict.TryGetValue("SpNumber", out var spNumber) && spNumber != null)
-                query = query.Where(x => EF.Functions.Like(x.SpNumber!.ToLower(), $"%{spNumber.ToString()!.ToLower()}%"));
+                query = query.Where(x => x.SpNumber != null && x.SpNumber.ToLower().Contains(spNumber.ToString()!.ToLower()));
 
             if (dict.TryGetValue("Name", out var name) && name != null)
-                query = query.Where(x => EF.Functions.Like(x.Name!.ToLower(), $"%{name.ToString()!.ToLower()}%"));
+                query = query.Where(x => x.Name != null && x.Name.ToLower().Contains(name.ToString()!.ToLower()));
 
             if (dict.TryGetValue("WorkGroupGrade", out var wgg) && wgg != null)
-                query = query.Where(x => EF.Functions.Like(x.WorkGroupGrade!.ToLower(), $"%{wgg.ToString()!.ToLower()}%"));
+                query = query.Where(x => x.WorkGroupGrade != null && x.WorkGroupGrade.ToLower().Contains(wgg.ToString()!.ToLower()));
 
             if (dict.TryGetValue("Title", out var title) && title != null)
-                query = query.Where(x => EF.Functions.Like(x.Title!.ToLower(), $"%{title.ToString()!.ToLower()}%"));
+                query = query.Where(x => x.Title != null && x.Title.ToLower().Contains(title.ToString()!.ToLower()));
 
             if (dict.TryGetValue("PersonStatus", out var personStatus) && personStatus != null)
-                query = query.Where(x => EF.Functions.Like(x.PersonStatus!.ToLower(), $"%{personStatus.ToString()!.ToLower()}%"));
+                query = query.Where(x => x.PersonStatus != null && x.PersonStatus.ToLower().Contains(personStatus.ToString()!.ToLower()));
 
             return query;
         }
