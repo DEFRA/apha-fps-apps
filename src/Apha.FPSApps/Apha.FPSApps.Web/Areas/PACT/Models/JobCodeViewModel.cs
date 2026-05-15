fbf5aa9f582cd0ccf5c95c5b2b0d5130447cdfb9
Pradeep-Niwate_Atos <pradeep.niwate@atos.net>
2026-05-15 08:36:17 +0000

    feat: Portfolio Time Codes JobCodeViewModel

--- src/Apha.FPSApps/Apha.FPSApps.Web/Areas/PACT/Models/JobCodeViewModel.cs
+++ src/Apha.FPSApps/Apha.FPSApps.Web/Areas/PACT/Models/JobCodeViewModel.cs
@@ -8,38 +8,28 @@ namespace Apha.FPSApps.Web.Areas.PACT.Models
     {
         [Display(Name = "Job Code")]
         [Required(ErrorMessage = "Job Code is required")]
-        [StringLength(50)]       
+        [StringLength(50)]
         [GridColumn(Order = 1, Width = 296, Type = GridColumnType.Text, IsFilterable = true)]
         public string JobCodeId { get; set; } = null!;
 
         [Display(Name = "Parent Project")]
-        [Required(ErrorMessage = "Project is required")]
         [StringLength(50)]
         [GridColumn(IsVisible = false)]
         public string? ParentProject { get; set; }
 
         [Display(Name = "Work Group")]
-        [StringLength(50)]       
+        [StringLength(50)]
         [GridColumn(Order = 4, Width = 296, Type = GridColumnType.Text, IsFilterable = true)]
         public string? JobCodeWorkGroup { get; set; }
 
         [Display(Name = "Type")]
-        [Required(ErrorMessage = "Type is required")]
-        [StringLength(15)]       
+        [StringLength(15)]
         [GridColumn(Order = 3, Width = 296, Type = GridColumnType.Text, IsFilterable = true)]
         public string? Type { get; set; }
 
         [Display(Name = "Job Code Name")]
-        [Required(ErrorMessage = "Name is required")]
-        [StringLength(255)]        
+        [StringLength(255)]
         [GridColumn(Order = 2, Width = 296, Type = GridColumnType.Text, IsFilterable = true)]
         public string? JobCodeName { get; set; }
-
-        [Display(Name = "Active")]
-        public bool Active { get; set; } = true;
-
-        [Display(Name = "Time Code")]
-        [StringLength(50)]
-        public string? TimeCode { get; set; }
     }
 }
