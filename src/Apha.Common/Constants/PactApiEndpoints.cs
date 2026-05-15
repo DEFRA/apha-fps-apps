2a2c76a41cfd9002c6c9fff1df541bd1fc10db5f
Pradeep-Niwate_Atos <pradeep.niwate@atos.net>
2026-05-15 09:01:52 +0000

    feat: Portfolio Time Codes

--- src/Apha.Common/Constants/PactApiEndpoints.cs
+++ src/Apha.Common/Constants/PactApiEndpoints.cs
@@ -17,6 +17,7 @@ namespace Apha.Common.Constants
         public const string GetTimeCodeValidById = "api/v1/timecodevalid/{0}/{1}/{2}";
         public const string GetPagedTimeCodes = "api/v1/timecodevalid/paged";
         public const string GetPagedTimeCodesByJobCode = "api/v1/timecodevalid/paged?jobCode={0}";
+        public const string GetPagedTimeCodesByProject = "api/v1/timecodevalid/paged?parentProject={0}";
         public const string GetPagedTimeCodesByTestCode = "api/v1/timecodevalid/paged?testCode={0}";
         public const string GetPagedTimeCodesByJobCodeAndProject = "api/v1/timecodevalid/paged?jobCode={0}&parentProject={1}";
         public const string GetPagedTimeCodesByProjectAndTestCode = "api/v1/timecodevalid/paged/project/{0}/testcode/{1}";
