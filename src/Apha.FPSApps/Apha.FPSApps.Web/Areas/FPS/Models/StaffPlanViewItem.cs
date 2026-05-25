e50f2640b16345168ed049b4e105284c2cf8a9c6
KEN0212JR <kenneth.rodrigues@atos.net>
2026-05-25 15:50:17 +0530

    feat : Add missing columns

--- src/Apha.FPSApps/Apha.FPSApps.Web/Areas/FPS/Models/StaffPlanViewItem.cs
+++ src/Apha.FPSApps/Apha.FPSApps.Web/Areas/FPS/Models/StaffPlanViewItem.cs
@@ -4,46 +4,46 @@ namespace Apha.FPSApps.Web.Areas.FPS.Models
 {
     public class StaffPlanViewItem
     {
-        [GridColumn(Width = 90, Type = GridColumnType.ReadOnly, IsFilterable = true)]
-        public string? ProgramNo { get; set; }
-
-        [GridColumn(Width = 120, Type = GridColumnType.ReadOnly, IsFilterable = true)]
+        [GridColumn(Order = 1, Width = 120, Type = GridColumnType.ReadOnly, IsFilterable = true)]
         public string? ParentProject { get; set; }
 
-        [GridColumn(Width = 120, Type = GridColumnType.ReadOnly, IsFilterable = true)]
+        [GridColumn(Order = 2, Width = 90, Type = GridColumnType.ReadOnly, IsFilterable = true)]
+        public string? ProgramNo { get; set; }
+
+        [GridColumn(Order = 3, Width = 120, Type = GridColumnType.ReadOnly, IsFilterable = true)]
         public string? Contract { get; set; }
 
-        [GridColumn(Width = 180, Type = GridColumnType.ReadOnly, IsFilterable = true)]
+        [GridColumn(Order = 4, Width = 180, Type = GridColumnType.ReadOnly, IsFilterable = true)]
         public string? Name { get; set; }
 
-        [GridColumn(Width = 80, Type = GridColumnType.ReadOnly, IsFilterable = true)]
+        [GridColumn(Order = 5, Width = 80, Type = GridColumnType.ReadOnly, IsFilterable = true)]
         public string? StaffId { get; set; }
 
-        [GridColumn(Width = 80, Type = GridColumnType.ReadOnly, IsFilterable = true)]
-        public string? WorkGroup { get; set; }
-
-        [GridColumn(Width = 70, Type = GridColumnType.ReadOnly, IsFilterable = true)]
-        public string? GradeCode { get; set; }
-
-        [GridColumn(Width = 80, Type = GridColumnType.ReadOnly)]
+        [GridColumn(Order = 6, Width = 90, Type = GridColumnType.ReadOnly)]
         public double? PlannedHours { get; set; }
 
-        [GridColumn(Width = 90, Type = GridColumnType.GbpValue)]
+        [GridColumn(Order = 7, Width = 90, Type = GridColumnType.GbpValue)]
         public decimal? ChargeRate { get; set; }
 
-        [GridColumn(Width = 90, Type = GridColumnType.GbpValue)]
+        [GridColumn(Order = 8, Width = 90, Type = GridColumnType.GbpValue)]
         public decimal? Cost { get; set; }
 
-        [GridColumn(Width = 90, Type = GridColumnType.GbpValue)]
+        [GridColumn(Order = 9, Width = 90, Type = GridColumnType.GbpValue)]
         public decimal? PayCost { get; set; }
 
-        [GridColumn(IsVisible = false)]
+        [GridColumn(Order = 10, Width = 100, Type = GridColumnType.ReadOnly, IsFilterable = true)]
         public string? ProfitCentre { get; set; }
 
-        [GridColumn(IsVisible = false)]
+        [GridColumn(Order = 11, Width = 90, Type = GridColumnType.ReadOnly, IsFilterable = true)]
+        public string? WorkGroup { get; set; }
+
+        [GridColumn(Order = 12, Width = 80, Type = GridColumnType.ReadOnly, IsFilterable = true)]
         public string? WgGrade { get; set; }
 
-        [GridColumn(IsVisible = false)]
+        [GridColumn(Order = 13, Width = 80, Type = GridColumnType.ReadOnly, IsFilterable = true)]
         public string? PcGrade { get; set; }
+
+        [GridColumn(Order = 14, Width = 70, Type = GridColumnType.ReadOnly, IsFilterable = true)]
+        public string? GradeCode { get; set; }
     }
 }
