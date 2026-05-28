3fc990bcba257b351d6459a8617b149ad5a2f7ab
Pradeep-Niwate_Atos <pradeep.niwate@atos.net>
2026-05-29 00:08:56 +0530

    feat: sonar issue fix

--- src/Apha.PACT/Apha.PACT.Application/Dtos/CopyInvoicesDto.cs
+++ src/Apha.PACT/Apha.PACT.Application/Dtos/CopyInvoicesDto.cs
@@ -5,6 +5,5 @@ namespace Apha.PACT.Application.Dtos
         public int SourceMonth { get; set; }
         public int TargetMonth { get; set; }
         public List<int>? InvoiceIds { get; set; }
-        public List<ProjectInvoiceDto>? InvoiceRecords { get; set; }
     }
 }
