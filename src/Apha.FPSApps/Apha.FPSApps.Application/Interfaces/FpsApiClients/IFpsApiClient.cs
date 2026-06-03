93a84d4f53ecb79e30e82d4429b0059147d44402
rohit2agarwal <rohit.2.agarwal@atos.net>
2026-06-03 06:39:41 +0000

    feat: initial for Project group implmentaton. (#115)

--- src/Apha.FPSApps/Apha.FPSApps.Application/Interfaces/FpsApiClients/IFpsApiClient.cs
+++ src/Apha.FPSApps/Apha.FPSApps.Application/Interfaces/FpsApiClients/IFpsApiClient.cs
@@ -22,5 +22,6 @@ namespace Apha.FPSApps.Application.Interfaces.FpsApiClients
         IFpsDivisionGradeApiClient FpsMaintDG { get; }
         IFpsProjectStaffPlanApiClient FpsProjectStaffPlan { get; }
         IFpsAnimalApiClient FpsAnimalMaster { get; }
+        IFpsProjectGroupApiClient FpsProjectGroup { get; }
     }
 }
