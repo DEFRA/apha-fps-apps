d3e56ca200005cc96509a82860b76931b451b98f
vishalnasta123 <vishal.nasta@atos.net>
2026-06-09 00:58:38 +0530

    #feature/FPS edit portfolio details unit test fixed

--- src/Apha.FPS/Apha.FPS.DataAccess.UnitTests/Repository/ProjectRepositoryTest/ProjectRepositoryTests.cs
+++ src/Apha.FPS/Apha.FPS.DataAccess.UnitTests/Repository/ProjectRepositoryTest/ProjectRepositoryTests.cs
@@ -1196,7 +1196,7 @@ namespace Apha.FPS.DataAccess.UnitTests.Repository.ProjectRepositoryTest
                     Customer = "DEFRA", ProjectStatus = "A", Disease = "D", Contract = "C", IncomeAccountCode = "IA"
                 }
             };
-            var repo = CreateRepository(projects: projects, fpsYear: 2024);
+            var repo = CreateRepository(projects: projects, projectLogs: new List<ProjectLog>(), fpsYear: 2024);
             var updated = new Project
             {
                 ParentProject = "PP001", ProjectTitle = "New Title", Program = "P002", Manager = "New Manager",
@@ -1297,7 +1297,7 @@ namespace Apha.FPS.DataAccess.UnitTests.Repository.ProjectRepositoryTest
                     Disease = "D", Contract = "C", IncomeAccountCode = "IA"
                 }
             };
-            var repo = CreateRepository(projects: projects, fpsYear: 2024);
+            var repo = CreateRepository(projects: projects, projectLogs: new List<ProjectLog>(), fpsYear: 2024);
             var updated = new Project
             {
                 ParentProject = "PP001", ProjectTitle = "Updated Title",
