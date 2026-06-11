94c396fd72e3a04d180177ce3fd7a493a7e969fc
Arihant Jain (atos.user8) <Arihant.ATOS-Jain@defra.gov.uk>
2026-06-11 22:36:11 +0530

    chore: remove temporary build and dbprobe artifacts

--- .tmp/verify-pact-api-build/Apha.BatchJobs.Infrastructure/Sql/RecreateSummaries/01_delete_fps_totals.sql
+++
@@ -1,5 +0,0 @@
--- 01_delete_fps_totals.sql
--- Replaces: sp_deleteFPSTotals
--- Syntax changes: dbo.FPSYearTotals -> fps.fpsyeartotals
-
-DELETE FROM fps.fpsyeartotals;
--- .tmp/verify-pact-api-build/Apha.BatchJobs.Infrastructure/Sql/RecreateSummaries/04_delete_time_cost_calcs.sql
+++
@@ -1,5 +0,0 @@
--- 04_delete_time_cost_calcs.sql
--- Replaces: sp_deleteTimeCostCalcs
--- Syntax changes: dbo.timecostcalcs -> fps.timecostcalcs
-
-DELETE FROM fps.timecostcalcs;
