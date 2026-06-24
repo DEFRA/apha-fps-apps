ca47974cb18c2341bdbc9949e00a8495a28531a2
Vishal Nasta <aaryanasta34@gmail.com>
2026-06-24 19:21:52 +0530

    Delete coverage/find-vla-methods.ps1

--- coverage/find-vla-methods.ps1
+++
@@ -1,7 +0,0 @@
-Select-String -Path "D:\FPS-Project-Profitability-VLA-Regression\coverage\fps-app\coverage.cobertura.xml" -Pattern "VLA" | Select-Object -First 10 | ForEach-Object { Write-Output $_.Line.Trim() }
-Write-Output "---fps-api---"
-Select-String -Path "D:\FPS-Project-Profitability-VLA-Regression\coverage\fps-api\coverage.cobertura.xml" -Pattern "VLA" | Select-Object -First 10 | ForEach-Object { Write-Output $_.Line.Trim() }
-Write-Output "---fpsapps-app---"
-Select-String -Path "D:\FPS-Project-Profitability-VLA-Regression\coverage\fpsapps-app\coverage.cobertura.xml" -Pattern "VLA" | Select-Object -First 10 | ForEach-Object { Write-Output $_.Line.Trim() }
-Write-Output "---fpsapps-infra---"
-Select-String -Path "D:\FPS-Project-Profitability-VLA-Regression\coverage\fpsapps-infra\coverage.cobertura.xml" -Pattern "VLA" | Select-Object -First 10 | ForEach-Object { Write-Output $_.Line.Trim() }
