ff092afabb91752ed577389e382dd17ca3ec97ad
VedpathakMeghana <meghana.vedpathak@atos.net>
2026-07-07 16:35:51 +0530

    feat: pims milestone changes

---
+++ src/Apha.Common/Contracts/PIMS/ProjectDetailsMilestoneRes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apha.Common.Contracts.PIMS
+{
+    public class ProjectDetailsMilestoneRes
+    {
+        public string Parentproject { get; set; } = null!;
+        public string? Program { get; set; }
+        public string? Customer { get; set; }
+        public string? ProjectGroup { get; set; }
+        public bool Formrequired { get; set; }
+
+        public char TypeLookUp { get; set; }
+    }
+}
