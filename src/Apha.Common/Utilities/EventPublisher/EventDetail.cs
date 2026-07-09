ec09a3e3e3442f7e0667b2c812935ffff908f063
AShamra1 <animesh.sharma@atos.net>
2026-07-09 14:03:55 +0530

    feat: decorate eventdetails class

--- src/Apha.Common/Utilities/EventPublisher/EventDetail.cs
+++ src/Apha.Common/Utilities/EventPublisher/EventDetail.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Apha.Common.Utilities.EventPublisher
 {
     public class EventDetail
     {
-        public string JobExecutionId { get; set; }
-        public string JobName { get; set; }
-        public string RunMode { get; set; }
+        [JsonPropertyName("jobExecutionId")]
+        public string JobExecutionId { get; set; } = string.Empty;
+
+        [JsonPropertyName("jobName")]
+        public string JobName { get; set; } = string.Empty;
+
+        [JsonPropertyName("runMode")]
+        public string RunMode { get; set; } = string.Empty;
+
+        [JsonPropertyName("requestedBy")]
         public string RequestedBy { get; set; } = string.Empty;     
 
+        [JsonPropertyName("requestedAtUtc")]
         public DateTime RequestedAtUtc { get; set; }
+
+        [JsonPropertyName("parametersJson")]
         public string? ParametersJson { get; set; }
     }
 }
