6c7a2a0d525ee1f062212182b23487ebdd6144d4
Kamlesh Daxini <kamlesh.daxini+Atos@atos.ai>
2026-07-12 18:52:22 +0530

    optimised scripts for liquibase cr28

--- dbscript/liquibase/changesets/cr028_add_constraint_job_queue.sql
+++ dbscript/liquibase/changesets/cr028_add_constraint_job_queue.sql
@@ -2,26 +2,15 @@
 
 --changeset repo-admin:CR028 labels:ddl context:all
 
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1
-        FROM pg_constraint con
-        INNER JOIN pg_class rel
-            ON rel.oid = con.conrelid
-        INNER JOIN pg_namespace nsp
-            ON nsp.oid = rel.relnamespace
-        WHERE con.conname = 'chk_job_queue_ended_at_utc_after_start'
-          AND nsp.nspname = 'fps'
-          AND rel.relname = 'job_queue'
-    ) THEN
-        ALTER TABLE fps.job_queue
-        ADD CONSTRAINT chk_job_queue_ended_at_utc_after_start CHECK (
-            ended_at_utc IS NULL OR ended_at_utc >= startdatetime
-        );
-    END IF;
-END $$;
+ALTER TABLE fps.job_queue
+DROP CONSTRAINT IF EXISTS chk_job_queue_ended_at_utc_after_start;
 
+ALTER TABLE fps.job_queue
+ADD CONSTRAINT chk_job_queue_ended_at_utc_after_start CHECK (
+    ended_at_utc IS NULL OR ended_at_utc >= startdatetime
+);
+
+--changeset repo-admin:CR028_cleanup labels:dml context:all
 DELETE FROM fps.job_lock
 WHERE lock_expires_at_utc < NOW();
 
