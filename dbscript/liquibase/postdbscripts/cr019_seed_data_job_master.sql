cac6e986567b0c4aa60721b757c4a05ab4ebd53a
Kamlesh Daxini <kamlesh.daxini+Atos@atos.ai>
2026-07-22 16:46:21 +0530

    removed somefiles for selective cr deployment to SIT

--- dbscript/liquibase/postdbscripts/cr019_seed_data_job_master.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
-
---changeset repo-admin:CR019 labels:ddl context:all
-
-UPDATE fps.job_master SET jobname='RecreateSummary', updated_at=NOW() WHERE jobname='RecreateSummaries';
-
---ROLLBACK
---Not Applicable
