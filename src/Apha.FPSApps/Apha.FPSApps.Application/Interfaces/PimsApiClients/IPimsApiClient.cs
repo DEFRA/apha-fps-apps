4941e4608e2cd2940e995e3914b5b8674ab1eed5
Sejal Dhotre <sejal.dhotre@atos.net>
2026-07-23 10:20:34 +0530

    feat: Removed comments from yearlyfinancial page.

--- src/Apha.FPSApps/Apha.FPSApps.Application/Interfaces/PimsApiClients/IPimsApiClient.cs
+++ src/Apha.FPSApps/Apha.FPSApps.Application/Interfaces/PimsApiClients/IPimsApiClient.cs
@@ -9,8 +9,7 @@
         IPimsProjectYearCostsApiClient PimsProjectYearCosts { get; }
         IPimsMilestoneApiClient PimsMilestone { get; }
         IPimsRadTrackInvoiceApiClient PimsRadTrackInvoice { get; }
-
-        // TRANSFORMENGINE: New sub-client for YearlyFinancialData CRUD + pactcosts endpoint
+        
         IPimsYearlyFinancialDataApiClient PimsYearlyFinancialData { get; }
     }
 }
