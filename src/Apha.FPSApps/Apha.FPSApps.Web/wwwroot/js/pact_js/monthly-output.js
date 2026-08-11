d50d89a83b79a66db9c6d91de14c136874d32f69
chougaleakshay <akshay.chougale@atos.net>
2026-08-11 15:17:03 +0000

    feat: Removed commented code

--- src/Apha.FPSApps/Apha.FPSApps.Web/wwwroot/js/pact_js/monthly-output.js
+++ src/Apha.FPSApps/Apha.FPSApps.Web/wwwroot/js/pact_js/monthly-output.js
@@ -683,22 +683,7 @@ function loadStagingModalBuyersByTestCode(workGroup, testCode, restoreBuyer) {
 }
 
 function saveStagingMonthlyOutput() {
-    var form = $('#stagingMonthlyOutputForm');
-
-    // Validate all numeric fields before checking isFormValid
-    // form.find('.decfmt-input').each(function() {
-    //     validateRangeOnInput(this);
-    // });
-
-    // Check for numeric validation errors
-    // if (hasNumericValidationErrors(form)) {
-    //     // Ensure validation messages are visible
-    //     if (typeof ensureValidationMessagesVisible === 'function') {
-    //         ensureValidationMessagesVisible(form);
-    //     }
-    //     displayClientValidationErrors(form, form);
-    //     return;
-    // }
+    var form = $('#stagingMonthlyOutputForm');   
 
     if (!isFormValid(form)) {
         displayClientValidationErrors(form, form);
