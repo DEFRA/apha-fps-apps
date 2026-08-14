71ca4abbc794360f16860065911c6bc8a88032e0
Nandkishor769 <nandkishor.diwakar@atos.net>
2026-08-14 13:14:33 +0530

    feat: Removed Comma in GridColumnType

--- src/Apha.FPSApps/Apha.FPSApps.Web/Models/Components/DataGrid/GridColumnType.cs
+++ src/Apha.FPSApps/Apha.FPSApps.Web/Models/Components/DataGrid/GridColumnType.cs
@@ -15,6 +15,6 @@
         GbpValueRounded, // £0 (rounded, no decimals)
         DoubleNumber,
         Percentage, // 0.00%
-        RoundTwoDecimal, // decimal/double/string-backed decimal, formatted as £#,##0.00
+        RoundTwoDecimal // decimal/double/string-backed decimal, formatted as £#,##0.00
     }
 }
