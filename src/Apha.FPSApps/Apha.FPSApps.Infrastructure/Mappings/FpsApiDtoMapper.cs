825383ae1f85de90119d4de139d6dbaa5e5e19de
Arihant Jain (atos.user8) <Arihant.ATOS-Jain@defra.gov.uk>
2026-08-25 10:32:52 +0530

    fix(sonar): remove duplicate using directives in FpsApiDtoMapper and FpsViewModelMapper

--- src/Apha.FPSApps/Apha.FPSApps.Infrastructure/Mappings/FpsApiDtoMapper.cs
+++ src/Apha.FPSApps/Apha.FPSApps.Infrastructure/Mappings/FpsApiDtoMapper.cs
@@ -2,7 +2,6 @@ using Apha.Common.Contracts;
 using Apha.Common.Contracts.FPS;
 using Apha.FPSApps.Application.Dtos;
 using Apha.FPSApps.Application.Dtos.FPS;
-using Apha.FPSApps.Application.Dtos.FPS;
 using Apha.FPSApps.Application.Pagination;
 using AutoMapper;
 namespace Apha.FPSApps.Infrastructure.Mappings
--- src/Apha.FPSApps/Apha.FPSApps.Web/Mappings/FpsViewModelMapper.cs
+++ src/Apha.FPSApps/Apha.FPSApps.Web/Mappings/FpsViewModelMapper.cs
@@ -1,6 +1,5 @@
 using Apha.FPSApps.Application.Dtos;
 using Apha.FPSApps.Application.Dtos.FPS;
-using Apha.FPSApps.Application.Dtos.FPS;
 using Apha.FPSApps.Application.Dtos.PACT;
 using Apha.FPSApps.Application.Pagination;
 using Apha.FPSApps.Web.Areas.FPS.Models;
