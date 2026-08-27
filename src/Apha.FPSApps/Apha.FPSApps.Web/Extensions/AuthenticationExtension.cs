d7ef29c8b26790f2f8e9ee407fc2e81e784d52ad
KEN0212JR <kenneth.rodrigues@atos.net>
2026-08-27 13:05:21 +0530

    feat: revert spacing changes

--- src/Apha.FPSApps/Apha.FPSApps.Web/Extensions/AuthenticationExtension.cs
+++ src/Apha.FPSApps/Apha.FPSApps.Web/Extensions/AuthenticationExtension.cs
@@ -10,18 +10,18 @@ namespace Apha.FPSApps.Web.Extensions
         public static IServiceCollection AddAuthenticationServices(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
         {
             var authBuilder = services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-             .AddMicrosoftIdentityWebApp(
-                 options =>
-                 {
-                     configuration.Bind("AzureAd", options);
+             .AddMicrosoftIdentityWebApp(options =>
+             {
+                 configuration.Bind("AzureAd", options);
 
-                     options.Events = new OpenIdConnectEvents
-                     {
-                         OnTokenValidated = context => HandleTokenValidatedAsync(context),
-                         OnRedirectToIdentityProvider = context => HandleRedirectToIdentityProvider(context),
-                         OnRemoteFailure = context => HandleRemoteFailure(context)
-                     };
-                 })
+                 options.Events = new OpenIdConnectEvents
+                 {
+                     OnTokenValidated = context => HandleTokenValidatedAsync(context),
+                     OnRedirectToIdentityProvider = context => HandleRedirectToIdentityProvider(context),
+                     OnRemoteFailure = context => HandleRemoteFailure(context)
+                 };
+
+             })
             .EnableTokenAcquisitionToCallDownstreamApi();
 
             if (!environment.IsEnvironment("local"))
