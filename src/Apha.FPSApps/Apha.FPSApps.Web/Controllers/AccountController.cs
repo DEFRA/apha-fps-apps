b7f85d8856ba16d0d4e94299ce54360943a7cf3b
KEN0212JR <kenneth.rodrigues@atos.net>
2026-08-27 13:04:14 +0000

    feat : removed fps specific code

--- src/Apha.FPSApps/Apha.FPSApps.Web/Controllers/AccountController.cs
+++ src/Apha.FPSApps/Apha.FPSApps.Web/Controllers/AccountController.cs
@@ -8,17 +8,6 @@ namespace Apha.FPSApps.Web.Controllers
     {
         public IActionResult AccessDenied(string? returnUrl)
         {
-            // If access was denied while trying to reach an FPS page,
-            // forward to the FPS-styled access denied page.
-            if (!string.IsNullOrEmpty(returnUrl) &&
-                returnUrl.StartsWith("/FPS/", StringComparison.OrdinalIgnoreCase))
-            {
-                return RedirectToAction(
-                    "AccessDenied",
-                    "Account",
-                    new { area = "FPS", returnUrl });
-            }
-
             return View();
         }
     }
