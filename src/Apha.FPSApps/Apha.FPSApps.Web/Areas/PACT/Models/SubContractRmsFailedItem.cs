51d0e2ec1f9d2be7b663fe5c8bab0b1808de75ef
chougaleakshay <akshay.chougale@atos.net>
2026-08-27 19:34:08 +0530

    feat: Removed workgroup and arranged propertiesas per order in failed item model.

--- src/Apha.FPSApps/Apha.FPSApps.Web/Areas/PACT/Models/SubContractRmsFailedItem.cs
+++ src/Apha.FPSApps/Apha.FPSApps.Web/Areas/PACT/Models/SubContractRmsFailedItem.cs
@@ -10,35 +10,31 @@ namespace Apha.FPSApps.Web.Areas.PACT.Models
         [GridColumn(Order = 1, Width = 120, Type = GridColumnType.Text)]
         public string? Project { get; set; }
 
-        [Display(Name = "Test Job")]
-        [GridColumn(Order = 5, Width = 110, Type = GridColumnType.Text)]
-        public string? TestJob { get; set; }
-
-        [Display(Name = "Month")]
-        [RegularExpression(@"^(?:[1-9]|1[0-2])$", ErrorMessage = "Month must be between 1 and 12.")]
-        [GridColumn(Order = 4, Width = 80, Type = GridColumnType.Text)]
-        public string? Month { get; set; }
+        [Display(Name = "Account Code")]
+        [GridColumn(Order = 2, Width = 120, Type = GridColumnType.Text)]
+        public string? AcctCode { get; set; }
 
         [Display(Name = "Amount")]
         [Range(typeof(decimal), "-999999999999999.9999", "999999999999999.9999", ErrorMessage = "Amount must be between -999,999,999,999,999.9999 and 999,999,999,999,999.9999.")]
         [GridColumn(Order = 3, Width = 100, Type = GridColumnType.RoundTwoDecimal)]
         public string? Amount { get; set; }
 
-        [Display(Name = "Work Group")]
-        [GridColumn(Order = 12, Width = 120, Type = GridColumnType.Text, IsVisible = false)]
-        public string? WorkGroup { get; set; }
+        [Display(Name = "Month")]
+        [RegularExpression(@"^(?:[1-9]|1[0-2])$", ErrorMessage = "Month must be between 1 and 12.")]
+        [GridColumn(Order = 4, Width = 80, Type = GridColumnType.Text)]
+        public string? Month { get; set; }
 
-        [Display(Name = "Account Code")]
-        [GridColumn(Order = 2, Width = 120, Type = GridColumnType.Text)]
-        public string? AcctCode { get; set; }
-
-        [Display(Name = "Supplier")]
-        [GridColumn(Order = 7, Width = 120, Type = GridColumnType.Text)]
-        public string? Supplier { get; set; }
+        [Display(Name = "Test Job")]
+        [GridColumn(Order = 5, Width = 110, Type = GridColumnType.Text)]
+        public string? TestJob { get; set; }
 
         [Display(Name = "Description")]
         [GridColumn(Order = 6, Width = 150, Type = GridColumnType.Text)]
         public string? Description { get; set; }
+
+        [Display(Name = "Supplier")]
+        [GridColumn(Order = 7, Width = 120, Type = GridColumnType.Text)]
+        public string? Supplier { get; set; }
 
         [Display(Name = "Supplier Number")]
         [Range(-2147483648, 2147483647, ErrorMessage = "Supplier Number must be a valid integer between -2,147,483,648 and 2,147,483,647.")]
