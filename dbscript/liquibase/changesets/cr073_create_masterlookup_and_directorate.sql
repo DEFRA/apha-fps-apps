9140115c7f3d4da09548f3c352b34b94af84b45b
Kamlesh Daxini <kamlesh.daxini+Atos@atos.ai>
2026-09-24 17:43:34 +0530

    repair cr073 by abhijeet in dev

--- dbscript/liquibase/changesets/cr073_create_masterlookup_and_directorate.sql
+++ dbscript/liquibase/changesets/cr073_create_masterlookup_and_directorate.sql
@@ -2,22 +2,20 @@
 
 --changeset abhijeet:CR073 labels:ddl context:all splitStatements:false
 
-CREATE TABLE IF NOT EXISTS fps.tblmasterlookup (
-    mastertablename varchar(100) NOT NULL,
-    CONSTRAINT pk_tblmasterlookup PRIMARY KEY (mastertablename)
-);
+CREATE TABLE IF NOT EXISTS fps.tblmasterlookup ( mastertablename varchar(100) NOT NULL, 
+CONSTRAINT pk_tblmasterlookup PRIMARY KEY (mastertablename));
 
-INSERT INTO fps.tblmasterlookup (mastertablename)
-VALUES ('tbldisease')
-ON CONFLICT (mastertablename) DO NOTHING;
+delete FROM fps.tblmasterlookup;
 
-INSERT INTO fps.tblmasterlookup (mastertablename)
-VALUES ('tlkpcustomer')
-ON CONFLICT (mastertablename) DO NOTHING;
+INSERT INTO fps.tblmasterlookup (mastertablename) VALUES
+    ('Disease');
 
-INSERT INTO fps.tblmasterlookup (mastertablename)
-VALUES ('tbldirectorate')
-ON CONFLICT (mastertablename) DO NOTHING;
+INSERT INTO fps.tblmasterlookup (mastertablename) VALUES
+    ('Customer');
+
+INSERT INTO fps.tblmasterlookup (mastertablename) VALUES
+    ('Directorate');
+
 
 CREATE TABLE IF NOT EXISTS fps.tbldirectorate
 (
@@ -32,6 +30,7 @@ WHERE directorate IS NOT NULL
   AND btrim(directorate) <> ''
 ON CONFLICT (directorate) DO NOTHING;
 
+
 DO $$
 BEGIN
     IF NOT EXISTS (
@@ -41,10 +40,10 @@ BEGIN
           AND conrelid = 'fps.tlkpprogram'::regclass
     ) THEN
        ALTER TABLE fps.tlkpprogram
-            ADD CONSTRAINT fk_tlkpprogram_directorate FOREIGN KEY (directorate)
-            REFERENCES fps.tbldirectorate (directorate) MATCH SIMPLE
-            ON UPDATE NO ACTION
-            ON DELETE NO ACTION;
+    		ADD CONSTRAINT fk_tlkpprogram_directorate FOREIGN KEY (directorate)
+        	REFERENCES fps.tbldirectorate (directorate) MATCH SIMPLE
+        	ON UPDATE NO ACTION
+        	ON DELETE NO ACTION;
     END IF;
 END $$;
 
